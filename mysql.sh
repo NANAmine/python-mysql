c4ab3852b84ae34ae44a66f8774bd96e081ecfd3
NANAmine <39719835+NANAmine@users.noreply.github.com>
2019-06-04 09:37:08 +0800

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -3,8 +3,8 @@
 import pymysql
  
 # 打开数据库连接
-db = pymysql.connect(host='39.105.221.247', port=3306, user='root', passwd='ljw3324199@', db='jeesite', charset='utf8')
-#db = pymysql.connect("39.105.221.247","root","ljw3324199@741","jeesite" )
+db = pymysql.connect(host='39.105.221.247', port=3306, user='root', passwd='数据库密码', db='jeesite', charset='utf8')
+#db = pymysql.connect("39.105.221.247","root","数据库密码","jeesite" )
  
 # 使用cursor()方法获取操作游标 
 cursor = db.cursor()
