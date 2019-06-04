aecfb546d59b35138812049d2ee610a761b7531b
NANAmine <1486496165@qq.com>
2019-06-04 09:30:44 +0800

    这是第一次提交

---
+++ mysql.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+ 
+import pymysql
+ 
+# 打开数据库连接
+db = pymysql.connect(host='39.105.221.247', port=3306, user='root', passwd='ljw3324199@', db='jeesite', charset='utf8')
+#db = pymysql.connect("39.105.221.247","root","ljw3324199@741","jeesite" )
+ 
+# 使用cursor()方法获取操作游标 
+cursor = db.cursor()
+ 
+# SQL 查询语句
+sql = "SELECT * FROM yp"
+try:
+   # 执行SQL语句
+   cursor.execute(sql)
+   # 获取所有记录列表
+   results = cursor.fetchall()
+   for row in results:
+       # 打印结果
+      print (row)
+except:
+   print ("Error: unable to fetch data")
+ 
+# 关闭数据库连接
+db.close()
